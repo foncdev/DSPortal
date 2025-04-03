12d982245458a3312f376c36316d32fdd6d3397f
kairu <kairu79@naver.com>
2025-04-03 10:22:36 +0900

    lint 환경파일 수정

--- packages/utils/.eslintrc.cjs
+++ packages/utils/.eslintrc.cjs
@@ -1,3 +1,14 @@
 module.exports = {
-    extends: ['@ds/eslint-config']
+    extends: ['@ds/eslint-config'],
+    parserOptions: {
+        project: './tsconfig.json', // app 디렉토리 내의 tsconfig.json 파일 참조
+        tsconfigRootDir: __dirname,  // 현재 디렉토리를 루트로 지정
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+        },
+    },
 };
